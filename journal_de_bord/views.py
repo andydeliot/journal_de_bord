7276ecac6f6ba94f1e6305fb235b7373ce93995e
andydeliot <A.andy@hotmail.fr>
2017-07-31 23:50:24 +0200

    Première vue

--- journal_de_bord/views.py
+++ journal_de_bord/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def MyFirstView(request):
+    return HttpResponse("<h1>Hello, world !</h1>")
+
