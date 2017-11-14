a574f2bfb27d188f766cb8675333575b27c9f788
andydeliot <A.andy@hotmail.fr>
2017-11-15 00:13:08 +0100

    Ordre affichage des journées

--- journal_de_bord/views.py
+++ journal_de_bord/views.py
@@ -5,7 +5,7 @@ from .models import Journee
 
 def home(request):
     """ Affiche toutes mes journées de mon journal de bord. """
-    journees = Journee.objects.all()
+    journees = Journee.objects.all().order_by("-jour")
     print(locals())
     return render(request, "journal_de_bord/home.html", locals())
 
